18d89e61a9fc916f286ad4665938dba74c0ecd3d
zzin0616 <zzin0616@naver.com>
2021-08-11 04:06:08 +0900

    0811

--- Android/app/src/main/java/org/techtown/catsby/community/Memo.java
+++ Android/app/src/main/java/org/techtown/catsby/community/Memo.java
@@ -1,2 +1,37 @@
-package org.techtown.catsby.community;public class Memo {
+package org.techtown.catsby.community;
+
+public class Memo {
+    String maintext; //메모
+    String subtext; //날짜
+    int isdone; //완료여부
+
+    public Memo(String maintext, String subtext, int isdone) {
+        this.maintext = maintext;
+        this.subtext = subtext;
+        this.isdone = isdone;
+    }
+    public String getMaintext() {
+        return maintext;
+    }
+
+    public void setMaintext(String maintext) {
+        this.maintext = maintext;
+    }
+
+    public String getSubtext() {
+        return subtext;
+    }
+
+    public void setSubtext(String subtext) {
+        this.subtext = subtext;
+    }
+
+    public int getIsdone() {
+        return isdone;
+    }
+
+    public void setIsdone(int isdone) {
+        this.isdone = isdone;
+    }
 }
+
--- Android/app/src/main/java/org/techtown/catsby/writemain.java
+++ Android/app/src/main/java/org/techtown/catsby/writemain.java
@@ -1,8 +1,12 @@
 package org.techtown.catsby;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class writemain extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class writemain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writemain);
+
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
