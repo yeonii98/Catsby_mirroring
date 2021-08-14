ad0b6097e4da905f2cc1f774342aee0da7d12265
zzin0616 <zzin0616@naver.com>
2021-08-15 02:22:30 +0900

    comment

--- Android/app/src/main/java/org/techtown/catsby/home/adapter/FeedAdapter.java
+++ Android/app/src/main/java/org/techtown/catsby/home/adapter/FeedAdapter.java
@@ -27,14 +27,14 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
         private ImageView bowlImg;
         private TextView userName;
         private ImageView feedImg;
-        private TextView content;
+        //private TextView content;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             bowlImg = itemView.findViewById(R.id.feed_bowlImg);
             userName = itemView.findViewById(R.id.feed_username);
             feedImg = itemView.findViewById(R.id.feed_img);
-            content = itemView.findViewById(R.id.feed_content);
+            //content = itemView.findViewById(R.id.feed_content);
 
         }
     }
@@ -54,9 +54,7 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
         holder.bowlImg.setImageResource(item.getBowlImg());
         holder.userName.setText(item.getUserName());
         holder.feedImg.setImageResource(item.getImg());
-        holder.content.setText(item.getContent());
-
-
+        //holder.content.setText(item.getContent());
     }
 
     @Override
