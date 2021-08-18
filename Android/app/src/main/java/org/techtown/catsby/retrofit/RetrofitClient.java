f164cd3a8a7cfa9323af85973c4c8417c5989715
wotj102@gmail.com <wotj102@gmail.com>
2021-08-18 23:26:30 +0900

    merge

--- Android/app/src/main/java/org/techtown/catsby/retrofit/RetrofitClient.java
+++ Android/app/src/main/java/org/techtown/catsby/retrofit/RetrofitClient.java
@@ -20,6 +20,7 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+
 public class RetrofitClient {
     private static final String BASE_URL = "http://192.168.45.200:8080/";
 
