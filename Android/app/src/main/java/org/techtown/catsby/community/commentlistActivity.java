0cdcb95fecd46e083de99a0180d2f9aba14e2a73
wotj102@gmail.com <wotj102@gmail.com>
2021-08-26 21:51:59 +0900

    merge

--- Android/app/src/main/java/org/techtown/catsby/community/commentlistActivity.java
+++ Android/app/src/main/java/org/techtown/catsby/community/commentlistActivity.java
@@ -28,7 +28,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class commentlistActivity extends AppCompatActivity {
+public class CommentlistActivity extends AppCompatActivity {
 
     Button commentUpdate;
     TextView commentText;
@@ -113,8 +113,6 @@ public class commentlistActivity extends AppCompatActivity {
                 System.out.println("t.getMessage() = " + t.getMessage());
             }
         });
-
-
     }
 
 
