b2cf71d5d6574cf0967ba1904f1934d1ac5ede21
wotj102@gmail.com <wotj102@gmail.com>
2021-08-29 02:45:02 +0900

    Splash 화면 등록

--- Android/app/src/main/java/org/techtown/catsby/cattown/FragmentCatTown.java
+++ Android/app/src/main/java/org/techtown/catsby/cattown/FragmentCatTown.java
@@ -74,7 +74,7 @@ public class FragmentCatTown extends Fragment {
         catlist = new ArrayList<>();
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://10.0.2.2:8080/")
+                .baseUrl("http://15.164.36.183:8080/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
