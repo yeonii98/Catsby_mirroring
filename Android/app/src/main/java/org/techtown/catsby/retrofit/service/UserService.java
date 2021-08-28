308a6a3afee6b8a4e991b7e52624b5b49dd87d85
안소정 <80646857+thwjd0504@users.noreply.github.com>
2021-08-28 20:41:17 +0900

    .

--- Android/app/src/main/java/org/techtown/catsby/retrofit/service/UserService.java
+++ Android/app/src/main/java/org/techtown/catsby/retrofit/service/UserService.java
@@ -3,13 +3,19 @@ package org.techtown.catsby.retrofit.service;
 import org.techtown.catsby.retrofit.dto.User;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface UserService {
 
     @GET("/user/{uid}")
     Call<User> getUser(@Path("uid") String uid);
+
+    @PUT("/user/address/{uid}")
+    Call<User> putUser(
+            @Field("uid") String uid);
 
 
 }
