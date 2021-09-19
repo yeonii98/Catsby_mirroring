c2d2ab8c360cd166c6d0308328916e5a77093b81
zzin0616 <zzin0616@naver.com>
2021-09-19 16:56:17 +0900

    .

--- Android/app/src/main/java/org/techtown/catsby/home/BowlActivity.java
+++ Android/app/src/main/java/org/techtown/catsby/home/BowlActivity.java
@@ -6,9 +6,15 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -17,6 +23,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -34,7 +41,11 @@ import org.techtown.catsby.retrofit.dto.BowlFeedList;
 import org.techtown.catsby.retrofit.dto.BowlLocation;
 import org.techtown.catsby.retrofit.service.BowlService;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -49,6 +60,8 @@ public class BowlActivity extends AppCompatActivity implements OnMapReadyCallbac
     private TextView bowlName, bowlLocation;
     private Button completedFeed;
 
+    private static final String TAG = "blackjin";
+
     private Boolean isPermission = true;
     private static final int PICK_FROM_ALBUM = 1;
 
@@ -57,6 +70,11 @@ public class BowlActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private NotificationService notificationService;
     private BowlService bowlService;
+
+    Uri photoUri;
+
+    File tempFile;
+    File image;
 
     Long bowlId;
     String name, address;
@@ -114,6 +132,56 @@ public class BowlActivity extends AppCompatActivity implements OnMapReadyCallbac
         loadBowlFeedTime(bowlId);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != Activity.RESULT_OK) {
+            Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_SHORT).show();
+
+            if (tempFile != null) {
+                if (tempFile.exists()) {
+                    if (tempFile.delete()) {
+                        Log.e(TAG, tempFile.getAbsolutePath() + " 삭제 성공");
+                        tempFile = null;
+                    }
+                }
+            }
+
+            return;
+        }
+
+        if (requestCode == PICK_FROM_ALBUM) {
+            photoUri = data.getData();
+            Log.d(TAG, "PICK_FROM_ALBUM photoUri : " + photoUri);
+
+            Cursor cursor = null;
+            try {
+
+                String[] proj = {MediaStore.Images.Media.DATA};
+                assert photoUri != null;
+                cursor = getContentResolver().query(photoUri, proj, null, null, null);
+                assert cursor != null;
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+
+                image = new File(cursor.getString(column_index));
+                tempFile = new File(cursor.getString(column_index));
+                //System.out.println("tempFile = " + tempFile);
+
+                Log.d(TAG, "tempFile Uri : " + Uri.fromFile(tempFile));
+
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+
+            setImage();
+
+        }
+    }
+
     private void sendNotification(Long bowlId, String uid) {
         notificationService.sendNotification(bowlId, uid).enqueue(new Callback<ApiResponse>() {
             @Override
@@ -137,6 +205,49 @@ public class BowlActivity extends AppCompatActivity implements OnMapReadyCallbac
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
         startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    /**
+     *  폴더 및 파일 만들기
+     */
+    private File createImageFile() throws IOException {
+
+        // 이미지 파일 이름 ( blackJin_{시간}_ )
+        String timeStamp = new SimpleDateFormat("HHmmss").format(new Date());
+        String imageFileName = "blackJin_" + timeStamp + "_";
+
+        // 이미지가 저장될 폴더 이름 ( blackJin )
+        File storageDir = new File(Environment.getExternalStorageDirectory() + "/blackJin/");
+        if (!storageDir.exists()) storageDir.mkdirs();
+
+        // 파일 생성
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        Log.d(TAG, "createImageFile : " + image.getAbsolutePath());
+        System.out.println("image = !!!!" + image);
+        return image;
+    }
+
+    /**
+     *  tempFile 을 bitmap 으로 변환 후 ImageView 에 설정한다.
+     */
+    private void setImage() {
+
+        //회전 방지
+        ImageView bowlimageView = findViewById(R.id.bowlimageView);
+        Glide.with(this).load(photoUri).into(bowlimageView);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        Bitmap originalBm = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
+        Log.d(TAG, "setImage : " + tempFile.getAbsolutePath());
+        bowlimageView.setImageBitmap(originalBm);
+
+        /**
+         *  tempFile 사용 후 null 처리를 해줘야 합니다.
+         *  (resultCode != RESULT_OK) 일 때 tempFile 을 삭제하기 때문에
+         *  기존에 데이터가 남아 있게 되면 원치 않은 삭제가 이뤄집니다.
+         */
+        tempFile = null;
+
     }
 
     private void loadBowlFeedTime(Long bowlId) {
