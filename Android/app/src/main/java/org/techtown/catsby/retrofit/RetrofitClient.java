db9c9a434d40272d62b0de5823668c71419f8a28
yeonii98 <ajy7424@naver.com>
2021-09-26 18:45:43 +0900

    우리동네 고양이 주소부분

--- Android/app/src/main/java/org/techtown/catsby/retrofit/RetrofitClient.java
+++ Android/app/src/main/java/org/techtown/catsby/retrofit/RetrofitClient.java
@@ -27,7 +27,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static final String BASE_URL =  "http://172.30.1.30:8080/";
-//private static final String BASE_URL = "http://10.0.2.2:8080/";
+
 
     private static Retrofit getInstance() {
         Gson gson = new GsonBuilder().setLenient().create();
