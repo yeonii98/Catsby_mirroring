f9daf7db347549d687ee625acf60e93c3b5288ab
yejiin <yj_song67@naver.com>
2021-10-04 04:38:10 +0900

    fix error

--- Server/src/main/java/com/hanium/catsby/domain/bowl/controller/BowlCommentController.java
+++ Server/src/main/java/com/hanium/catsby/domain/bowl/controller/BowlCommentController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
